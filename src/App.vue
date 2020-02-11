6962f391467fa93e2c2e32cfe83279d286428177
if0balino <balino.ical@gmail.com>
2020-02-11 19:54:42 +0700

    git pertama

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app>
+    <v-card width="400" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">Login</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field 
+            label="Username"
+            prepend-icon="mdi-account-circle"
+          />
+          <v-text-field
+            :type="showPassword ? 'text' : 'Password'"
+            label="Password"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' :
+            'mdi-eye-off'"
+            @click:append="showPassword = 
+            !showPassword"
+          />
+        </v-form>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-action>
+        <v-btn width="100%" color="success">Register</v-btn>
+        <!-- <v-spacer></v-spacer> -->
+        <v-btn width="100%" color="info" >Login</v-btn>
+        <!-- <tes></tes> -->
+      </v-card-action>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+  components: {
+    // 'tes': HelloWorld,
+  },
+  data: () => ({
+    showPassword: false
+  }),
+};
+</script>
